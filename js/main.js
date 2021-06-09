052b4fa67d1e5501a651bc734703b753034bd6a6
Tyler McCarthy <tweedlitweedli@gmail.com>
2021-06-08 22:25:51 -0500

    js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,72 @@
 import { getSentence } from './requests.js'
 
 ;(async () => {
-    // build ui
+    const resultBox = document.getElementById('result-box')
     const form = document.getElementById('submission-form')
+
     const resultField = document.getElementById('submission-result')
     const errorField = document.getElementById('submission-error')
+    const loadingText = document.getElementById('loading-text')
 
     const subject = document.getElementById('subject-prompt')
     const verb = document.getElementById('verb-prompt')
     const object = document.getElementById('object-prompt')
 
+    const errorTexts = {
+        subject: document.getElementById('subject-small'),
+        verb: document.getElementById('verb-small'),
+        object: document.getElementById('object-small'),
+        tense: document.getElementById('tense-small')
+    }
+
+    const handleErrors = (errorString) => {
+        errorString
+            .split('is null.')
+            .map(item => item.trim())
+            .map(item => {
+                item && errorTexts[item] != null ? errorTexts[item].classList.remove('hidden') : null
+            })
+    }
+
+    // on submitting of the form, we:
+    // hide all error texts
+    // send our requests to the API.
+    // if successful, we display the resulting sentence.
+    // if there's an error, we alert the user to which field the error is on.
     form.onsubmit = async (event) => {
         event.preventDefault()
 
-        const tense = document.querySelector('input[name="tense"]:checked').value
-        console.log(tense)
+        for (let key in errorTexts) {
+            errorTexts[key].classList.add('hidden')
+        }
 
-        const res = await getSentence(subject.value, verb.value, object.value, tense)
-        console.log(await res.json())
+        resultBox.classList.remove('display-none')
+        loadingText.classList.remove('display-none')
+        errorField.classList.add('display-none')
+        resultField.classList.add('display-none')
 
-        // scroll to top!
-        // handle
-            // loading
-            // error (scroll to validation?)
-            // results
+        try {
+            const tense = document.querySelector('input[name="tense"]:checked')
+            if (!tense) {
+                handleErrors('tense is null.')
+                loadingText.classList.add('display-none')
+                errorField.classList.remove('display-none')
+                return
+            }
+
+            const response = await getSentence(subject.value, verb.value, object.value, tense.value)
+            const result = await response.json()
+
+            loadingText.classList.add('display-none')
+            if (result.result === 'OK') {
+                resultField.classList.remove('display-none')
+                resultField.innerText = result.sentence
+            } else {
+                handleErrors(result.error_message)
+                errorField.classList.remove('display-none')
+            }
+        } catch (e) {
+            console.error(e)
+        }
     }
 })()
--- js/requests.js
+++ js/requests.js
@@ -1,16 +1,11 @@
 const BASE_TEST_URL = 'https://lt-nlgservice.herokuapp.com/rest/english/realise'
 
-export const getSentence = (subject, verb, object, tense = null) => {
+export const getSentence = (subject, verb, object, tense) => {
     const params = new URLSearchParams()
     params.append('subject', subject)
     params.append('verb', verb)
     params.append('object', object)
-
-    if (tense) {   
-        params.append('tense', tense)
-    }
-
-    console.log(params.toString())
+    params.append('tense', tense)
 
     return fetch(`${BASE_TEST_URL}?${params.toString()}`)
 }
